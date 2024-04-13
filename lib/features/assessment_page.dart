f78fa6155f010a597920cb2b76ec58cee0afe418
nhacnt17 <nhacntpd06031@fpt.edu.vn>
2024-04-13 13:54:11 +0700

    Assessments fix ui

--- lib/features/assessment_page.dart
+++ lib/features/assessment_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // For date formatting
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart'; // Để định dạng ngày tháng
 
 class Assessment {
   const Assessment({
@@ -22,100 +24,156 @@ class Assessments extends StatefulWidget {
 
 class _AssessmentsState extends State<Assessments> {
   List<Assessment> _assessments = [
-    Assessment(title: "5D 2024, Term 2", className: "5D", date: DateTime(2024, 5, 2)),
-    Assessment(title: "5D 2024, Term 2", className: "5D ", date: DateTime(2024, 5, 2)),
-    Assessment(title: "5D 2024, Term 2", className: "5D", date: DateTime(2024, 5, 2)),
-    Assessment(title: "5D 2024, Term 2", className: "5D", date: DateTime(2024, 5, 2)),
-    // ... add more assessments if needed
+    Assessment(title: "5D 2024, Học kỳ 2", className: "5D", date: DateTime(2024, 5, 2)),
+    Assessment(title: "5D 2024, Học kỳ 2", className: "5D ", date: DateTime(2024, 5, 2)),
+    Assessment(title: "5D 2024, Học kỳ 2", className: "5D", date: DateTime(2024, 5, 2)),
+    Assessment(title: "5D 2024, Học kỳ 2", className: "5D", date: DateTime(2024, 5, 2)),
+    // ... thêm các dữ liệu khác nếu cần
   ];
 
-  String _searchText = ""; // For search functionality
+  String _searchText = ""; // Để tìm kiếm chức năng
 
   @override
   Widget build(BuildContext context) {
+    // Lấy kích thước của màn hình
+    Size screenSize = MediaQuery.of(context).size;
+
+    // Kiểm tra nếu là máy tính (web)
+    bool isWeb = screenSize.width > 600;
+
+    // Thiết lập kích thước cho widget dựa trên điều kiện
+    double widgetWidth = isWeb ? 400 : screenSize.width;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF33A849), // Green color
-        title: Text(
-          "ASSESSMENTS",
-          style: TextStyle(
-            color: Colors.white, // White text
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        automaticallyImplyLeading: false, // Remove back button
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 4, // Give more space to the search field
-            child: TextField(
-              decoration: const InputDecoration(
-                hintText: "Search by assessment title...",
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(16.0),
+      body:Center(
+        child: Container(
+          height: screenSize.height,
+          width: widgetWidth,
+          color: Color(0xFFF6F6F6),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.green,
+                    margin: EdgeInsets.only(bottom: 50),
+                    height: 50,
+                    width: widgetWidth,
+                    child: Center(
+                      child: Text(
+                        "ASSESSMENTS",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 30),
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.green),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          isDense: true,
+                          hintText: "Search by assessment title...",
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.green),
+                        ),
+                        onChanged: (text) {
+                          setState(() {
+                            _searchText = text.toLowerCase(); // Tìm kiếm không phân biệt chữ hoa chữ thường
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24, right: 24,top: 24,bottom: 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded (
+                          flex: 2,
+                          child: Container(
+                            margin: EdgeInsets.only(left: 5),
+                            child: Text(
+                              'Title',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Class',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Task Date',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // Các thẻ đánh giá hiện có của bạn
+                  for (final assessment in _assessments)
+                    _assessmentCard(assessment),
+                ],
               ),
-              onChanged: (text) {
-                setState(() {
-                  _searchText = text.toLowerCase(); // Search case-insensitive
-                });
-              },
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround, // Distribute labels evenly
-            children: [
-              Text(
-                'Title',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Class',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Task Date',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16.0), // Add some spacing below the labels
-
-          // Your existing assessment cards
-          for (final assessment in _assessments)
-            _assessmentCard(assessment),
-        ],
       ),
     );
   }
   Widget _assessmentCard(Assessment assessment) {
     return Card(
+      color: Colors.white,
       margin: const EdgeInsets.all(8.0),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           children: [
             Expanded(
-              flex: 2, // Set the width of the Title column
-              child: Text(assessment.title),
+              flex: 2,
+              child: Container(
+                  child: Text(assessment.title)),
             ),
             Expanded(
-              flex: 1, // Set the width of the Class column
-              child: Text(assessment.className),
+              flex: 1,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text(assessment.className)),
             ),
             Expanded(
-              flex: 1, // Set the width of the Task Date column
-              child: Text(assessment.date.formatDate()),
+              flex: 1,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text(assessment.date.formatDate())),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '', // Đặt title thành chuỗi rỗng để loại bỏ tiêu đề
       theme: ThemeData(
         primarySwatch: Colors.blue,
